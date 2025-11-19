749e940e472bb5751e08d40b8a7c8270fbf3542d
sanjeev <sanjeevlee2004@gmail.com>
2025-11-19 13:17:06 +0530

    Remove .next from tracking

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"components/analytics/WebVitals.tsx -> web-vitals\":{\"id\":\"components/analytics/WebVitals.tsx -> web-vitals\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_web-vitals_dist_web-vitals_js.js\"]}}"
